544d07a1c0fe744cd58271e511b0820af87305ba
sonyujin <jainayj804998@gmail.com>
2023-10-16 01:18:40 +0900

    Update App.tsx : Setup Page Route using createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Router = createBrowserRouter([
+  {
+      path: '/',
+      element: <></>,
+  },
+  {
+    path: '/login',
+    element: <></>,
+},
+  {
+      path: '/chat',
+      element: <></>,
+  },
+  {
+      path: '/roomate',
+      element: <></>,
+  },
+  {
+      path: '/group',
+      element: <></>,
+  },
+  {
+    path: '/notice',
+    element: <></>,
+},
+{
+    path: '/testpage',
+    element: <></>,
+},
+{
+    path: '/testpage',
+    element: <></>,
+},
+])
 
-export default App;
+export const ProjectRouter = () => <RouterProvider router={Router} />
