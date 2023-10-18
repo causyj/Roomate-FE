038b64acc9eba2e6b7c837489471e24be862feed
sonyujin <jainayj804998@gmail.com>
2023-10-18 20:23:50 +0900

    Added roomatelist: roomate page components

---
+++ src/pages/roomatelist/RoomateFavorite.tsx
@@ -0,0 +1,5 @@
+export function RoomateFavorite() {
+    return (
+        <div>찜한 룸메이트를 보여주는 페이지입니다.</div>
+    )
+}
---
+++ src/pages/roomatelist/RoomateRecommend.tsx
@@ -0,0 +1,5 @@
+export function RoomateRecommend() {
+    return (
+        <div>추천한 룸메이트를 보여주는 페이지입니다.</div>
+    )
+}
