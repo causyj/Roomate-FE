a3653fb1e53d310ea7782d20ee8a4252fa43c870
sonyujin <jainayj804998@gmail.com>
2023-10-18 20:17:36 +0900

    Added group page components

---
+++ src/pages/group/GroupDetail.tsx
@@ -0,0 +1,5 @@
+export function GroupDetail() {
+    return (
+        <div>모임의 상세 페이지를 보여주는 페이지입니다</div>
+    )
+}
---
+++ src/pages/group/GroupList.tsx
@@ -0,0 +1,5 @@
+export function AnimalDictionary() {
+    return (
+        <div>모임의 전체목록을 보여주는 페이지 입니다</div>
+    )
+}
