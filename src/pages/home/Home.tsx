910adf876d68689d1f26b20233269ac81d0fd388
sonyujin <jainayj804998@gmail.com>
2023-10-27 21:12:51 +0900

    Update index.css : setup font Pretendard

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,10 +4,20 @@ import Box from '@mui/material/Box';
 export function Home() {
     return (
         <div>
-            <div>안녕 서비스임</div>
-            <Box sx={{ display: 'flex' }}>
+            <div className='font-bold'>세상에 이런 폰트가 d나오다니 천재인듯</div>
+            
+            <div className="font-['100']">안녕 서비스임 </div>
+            <div className="font-['200']">안녕 서비스임 </div>
+            <div className="font-['300']">안녕 서비스임</div>ㅓ
+            <div className="font-['400']">안녕 서비스임7</div>
+            <div className="font-['500']">안녕 서비스임7</div>
+            <div className="font-['600']">안녕 서비스임7</div>
+            <div className="font-['700']">안녕 서비스임7</div>
+            <div className="font-['800']">안녕 서비스임7</div>
+            <div className="font-['900']">안녕 서비스임7</div>
+            {/* <Box sx={{ display: 'flex' }}>
                 <CircularProgress />
-            </Box>
+            </Box> */}
         </div>
     )
 }
