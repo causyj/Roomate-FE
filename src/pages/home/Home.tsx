43f0511c8cdf0a09e1ea7cbdf52687fc84d424dc
sonyujin <jainayj804998@gmail.com>
2023-10-28 23:03:54 +0900

    Update .css: added default font color and common color&font size

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,18 +6,15 @@ export function Home() {
         <div>
             <div className='font-bold'>세상에 이런 폰트가 d나오다니 천재인듯</div>
             
-            <div className="font-['100']">안녕 서비스임 </div>
-            <div className="font-['200']">안녕 서비스임 </div>
-            <div className="font-['300']">안녕 서비스임</div>ㅓ
+            <div className="font-['100']">Hi this is test For eng.ver </div>
+            <div className="font-['200'] text-primary-sd">Typography </div>
+            <div className="font-['300'] ">안녕 서비스임</div>
             <div className="font-['400']">안녕 서비스임7</div>
             <div className="font-['500']">안녕 서비스임7</div>
             <div className="font-['600']">안녕 서비스임7</div>
             <div className="font-['700']">안녕 서비스임7</div>
             <div className="font-['800']">안녕 서비스임7</div>
             <div className="font-['900']">안녕 서비스임7</div>
-            {/* <Box sx={{ display: 'flex' }}>
-                <CircularProgress />
-            </Box> */}
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,36 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+        fontSize: {
+            '8xl': '6rem',
+            '6xl': '3.75rem',
+            '5xl': '3rem',
+            '3xl': '1.875rem',
+            '2xl': '1.5rem',
+            'xl' : '1.25rem',
+            'xxs': '0.625rem',
+            '3xs': '0.5rem',
+            '5xs': '0.1rem',
+        },
+        colors: {
+          theme: {
+              background: '#18181B',
+              font: '#D92929',
+          },
+          primary: {
+              yellow: '#FFE247',
+              hotpink: '#FF3C76',
+              teal: '#00ECC1',
+              blue: '#1753F1',
+              purple: '#946AFE',
+              darkgray: '#18181B',
+              whitegray: '#D9D9D9',
+              logo: '#656C73',
+          },
+      },
+    },
+},
   plugins: [],
 }
 
