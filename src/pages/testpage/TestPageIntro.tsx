94bb227ee40baf9eb646004e3cb02f5942b29ade
sonyujin <jainayj804998@gmail.com>
2023-10-29 21:06:57 +0900

    Updated TestPageIntro.tsx: 1st page except button

--- src/pages/testpage/TestPageIntro.tsx
+++ src/pages/testpage/TestPageIntro.tsx
@@ -1,16 +1,27 @@
+import { relative } from "path"
+
 const GoToTest = () => {
     return(
         <div className="flex flex-col p-12">
-            <div className="font-['700'] text-3xl mt-6 mb-4">
+            <div className="font-['700'] text-3xl mt-12 mb-6">
                 <div>어서오세요!</div>
                 <div>처음이신가요?</div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap text-l">
                 <div>나와 맞는 룸메이트를 찾기 위해 먼저</div>
                 <div className="font-['600']">나의 생활유형</div>
                 <div>을 알아야해요!</div>
             </div>
-            
+            <div className='flex justify-end items-end mt-56 mb-4' style={{position:'relative'}}>
+                <img src={process.env.PUBLIC_URL + '/cloud.png'} alt="TalkBaloon" style={{width : '200px', position:'absolute' }}/>
+                <div className="flex flex-col items-center mb-24 pr-8 font-['700'] text-xl" style={{position : 'absolute'}}>
+                    <div className="mb-2">루미랑</div>
+                    <div >테스트 하러갈래?</div>
+                </div>
+            </div>
+            <div>
+                <img src={process.env.PUBLIC_URL + '/roomie.png'} alt="Roomie" style={{width : '180px'}} />
+            </div>
         </div>
     )
 }
