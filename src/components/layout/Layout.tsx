a186b7cc95a28413651e87c4c354928b8e4c969c
sonyujin <jainayj804998@gmail.com>
2023-10-29 23:34:13 +0900

    Added styled-components

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+
+const BaseLayout = () => {
+    return (
+        <div></div>
+    )
+};
+
+// export const Layout = ({ children }: React.PropsWithChildren) => {
+//     return (
+//         <main className="min-h-screen max-h-screen h-screen w-full bg-white flex items-center justify-center">
+//             {children}
+//         </main>
+//     )
+// }
+
+export default BaseLayout;
