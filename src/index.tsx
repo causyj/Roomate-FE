ed12abd543fbe8487024817375a04ab3d0c90b62
sonyujin <jainayj804998@gmail.com>
2023-10-30 00:17:30 +0900

    Update files

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ProjectRouter } from './App';
-<<<<<<< HEAD
-import { BaseLayout } from './components/layout/Layout';
-=======
-import BaseLayout from './components/Layout/BaseLayout';
->>>>>>> 2725470 (Added BaseLayout.tsx)
+import {BaseLayout} from './components/layout/BaseLayout';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BaseLayout>
-<<<<<<< HEAD
-        <ProjectRouter />
-=======
       <ProjectRouter />
->>>>>>> 2725470 (Added BaseLayout.tsx)
     </BaseLayout>
    </React.StrictMode>
 );
