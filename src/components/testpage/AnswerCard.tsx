aaa70798807a57fac2c505437d97ff761e1bda0e
sonyujin <jainayj804998@gmail.com>
2023-11-01 20:11:53 +0900

    Added AnswerCard.tsx:added testpage answer card

--- src/components/testpage/AnswerCard.tsx
+++ src/components/testpage/AnswerCard.tsx
@@ -1,5 +1,9 @@
 export function AnswerCard() {
     return (
-        <div className="border-2 border-color text-primary-bg">한번에 일어난다.</div>
+        <div>
+            <div className="w-full h-24 border-2 border-stone-500 border-color-black-800 rounded-lg text-primary-bgitems-center justify-center text-center p-8 text-['700'] text-xl font-bold">한줄일때</div>
+            <div className="mt-12"></div>
+            <div className="w-full h-24 border-2 border-stone-500 border-color-black-800 rounded-lg text-primary-bgitems-center justify-center text-center p-5 text-['700'] text-xl font-bold">두줄이상 넘어가면 p-6을 사용해서 쓰면 될 것 같습니다.</div>
+        </div>
     )
 }
