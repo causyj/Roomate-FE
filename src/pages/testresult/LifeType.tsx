5ed26aeb0948b2d5d3452910d4ab2350248cc776
sonyujin <jainayj804998@gmail.com>
2023-11-04 04:32:57 +0900

    Updated TypeEmoiji.tsx: added good or bad roomate

--- src/pages/testresult/LifeType.tsx
+++ src/pages/testresult/LifeType.tsx
@@ -53,15 +53,12 @@ const ResultDetail = () => {
 export const LifeType =() => {
     return (
         <div>
-            <div className="text-center text-2xl font-['800']">
-                <div className='mb-4 mt-4'>모글리님의 생활 유형은, </div>
-                <span className='text-5xl text-primary-orange'>원숭이</span> 입니다.
+            <div className="text-center text-2xl font-['800'] items-center justify-center">
+                <div className='mb-4 mt-4'>모글리님의 생활 유형은</div>
+                <span className='text-4xl text-primary-orange'>둔감한 원숭이</span> 
             </div>
          <AnimalDescription />
-        <div className=" flex justify-center ">
-            <img src={process.env.PUBLIC_URL + '/scroll.png'} alt="scroll"  />
-        </div>
-        <div className="mt-12"> <TitleBox title={'모글리님의 생활 유형 한 눈에 보기'}/></div>
+        <div className="mt-8"> <TitleBox title={'모글리님의 생활 유형 한 눈에 보기'}/></div>
         <TypeAtAGlance />
         <TitleBox title={'모글리님 생활 유형 결과'}/>
         <ResultDetail />
--- src/pages/testresult/components/TypeEmoji.tsx
+++ src/pages/testresult/components/TypeEmoji.tsx
@@ -1,3 +1,5 @@
+import FavoriteOutlined from "@mui/icons-material/FavoriteOutlined"
+import BrokenHeart from "@mui/icons-material/HeartBrokenOutlined"
 import { TitleBox } from "./TitleBox"
 export const TypeEmoji=() => {
     return(
@@ -10,13 +12,50 @@ export const TypeEmoji=() => {
                 </div>
                 <div className="flex flex-col contain mr-8 ">
                     <img src={process.env.PUBLIC_URL + '/nosmoking.png'} alt="morning"  />
-                    <div className="font-['700'] text-primary-gray">아침형</div>
+                    <div className="font-['700'] text-primary-gray">비흡연</div>
                 </div>
                 <div className="flex flex-col contain ">
                     <img src={process.env.PUBLIC_URL + '/homelover.png'} alt="morning"  />
-                    <div className="font-['700'] text-primary-gray">아침형</div>
+                    <div className="font-['700'] text-primary-gray">집순이</div>
                 </div>
-                
+            </div>
+            <div className="flex flex-row justify-evenly">
+                <div className="flex flex-col text-center">
+                    <div className="w-40 mt-4 h-8 border-4 border-primary-orange rounded-lg flex items-center justify-center p-8 ">
+                        <img src={process.env.PUBLIC_URL + '/good.png'} alt="morning"  />
+                    </div>
+                    <div className=" mt-1 font-['700'] text-sm">이런 룸메와 잘 맞아요</div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-4">
+                    <div className=" text-primary-orange"><FavoriteOutlined/></div>
+                        <div className="ml-2">고릴라</div>
+                    </div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-1">
+                        <div className=" text-primary-orange"><FavoriteOutlined/></div>
+                        <div className="ml-2 ">고양이</div>
+                    </div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-1">
+                    <div className=" text-primary-orange"><FavoriteOutlined/></div>
+                        <div className="ml-2">강아지</div>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <div className="w-40 mt-4 h-8 border-4 border-primary-orange rounded-lg flex items-center justify-center p-8 ">
+                        <img src={process.env.PUBLIC_URL + '/bad.png'} alt="morning"  />
+                    </div>
+                    <div className="text-center mt-1 font-['700'] text-sm">이런 룸메와는 조금 힘들어요</div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-4">
+                    <div className=" text-primary-orange"><BrokenHeart/></div>
+                        <div className="ml-2">고릴라</div>
+                    </div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-1">
+                        <div className=" text-primary-orange"><BrokenHeart/></div>
+                        <div className="ml-2 ">고양이</div>
+                    </div>
+                    <div className="flex flex-row text-center font-['700'] items-center justify-center mt-1">
+                    <div className=" text-primary-orange"><BrokenHeart/></div>
+                        <div className="ml-2">강아지</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
