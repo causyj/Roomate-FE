0aedd3a510ee0a67582f6f60a113f4d5b6857ad2
sonyujin <jainayj804998@gmail.com>
2023-11-03 12:25:29 +0900

    Added ProgressIndLinear.tsx

--- src/pages/testresult/TestResultLoading.tsx
+++ src/pages/testresult/TestResultLoading.tsx
@@ -1,6 +1,7 @@
+import ProgressIndLinear from "../../components/common/ProgressIndLinear"
 export function TestResultLoading() {
     return (
-      <div className="w-full">
+      <div className="">
             <div className="font-['800'] text-2xl mt-16 ">
             모글리님의
             <div>생활 유형을 분석 중이에요</div>
@@ -9,6 +10,7 @@ export function TestResultLoading() {
             <div className="mt-8 w-full">
               <img src={process.env.PUBLIC_URL + '/puang1.png'} alt="puang1" className="w-full h-auto" />
           </div>
+          <div className="mt-8"><ProgressIndLinear /></div>
       </div>
     )
 }
