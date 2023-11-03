fe25d04648a19fe335826d116fe00505b2c8a7f5
sonyujin <jainayj804998@gmail.com>
2023-11-04 01:08:18 +0900

    Updated LifeType.tsx: Added type result description

--- src/pages/testresult/LifeType.tsx
+++ src/pages/testresult/LifeType.tsx
@@ -31,20 +31,39 @@ const TypeAtAGlance = () =>{
       </div>
     )
 }  
+const ResultDetail = () => {
+    return(
+        <div className="p-8">
+            <div className="font-['800'] mb-2">모글리님은 이런 생활 유형이에요!</div>
+            <div className="font-['700'] text-primary-gray mb-8">: 밤 12시에 취침하고, 오전 8시에 일어나요.매주 한 번의 방청소를 통해 깨끗한 환경을 유지하며,
+                    씻는 시간도 아침으로 일정하고 적절한 시간동안만 해요. 음식을 주로 방에서 섭취하며, 비흡연자에요
+                    도서관에서 주로 공부하고, 노트북을 자주 사용해요 .룸메이트와 친하게 지내고 싶어해요. 잠버릇은 좋아요 
+            </div>
+            <div className="font-['800'] mb-2">모글리님이 원하는 룸메이트 스타일은?</div>
+            <div className="font-['700'] text-primary-gray">: 밤 12시에 취침하고, 오전 8시에 일어나요.매주 한 번의 방청소를 통해 깨끗한 환경을 유지하며,
+                    씻는 시간도 아침으로 일정하고 적절한 시간동안만 해요. 음식을 주로 방에서 섭취하며, 비흡연자에요
+                    도서관에서 주로 공부하고, 노트북을 자주 사용해요 .룸메이트와 친하게 지내고 싶어해요. 잠버릇은 좋아요 
+            </div>
+        </div>
+            
+    )
+}
 
 export const LifeType =() => {
     return (
         <div>
             <div className="text-center text-2xl font-['800']">
-            <div className='mb-4 mt-4'>모글리님의 생활 유형은, </div>
-            <span className='text-5xl text-primary-orange'>원숭이</span> 입니다.
-        </div>
+                <div className='mb-4 mt-4'>모글리님의 생활 유형은, </div>
+                <span className='text-5xl text-primary-orange'>원숭이</span> 입니다.
+            </div>
          <AnimalDescription />
         <div className=" flex justify-center ">
             <img src={process.env.PUBLIC_URL + '/scroll.png'} alt="scroll"  />
         </div>
-        <TitleBox title={'나의 생활 유형 한 눈에 보기'}/>
+        <div className="mt-12"> <TitleBox title={'모글리님의 생활 유형 한 눈에 보기'}/></div>
         <TypeAtAGlance />
+        <TitleBox title={'모글리님 생활 유형 결과'}/>
+        <ResultDetail />
         </div>
     )
 } 
--- src/pages/testresult/components/TitleBox.tsx
+++ src/pages/testresult/components/TitleBox.tsx
@@ -1,6 +1,6 @@
 export function TitleBox({title }: { title: string }) {
     return (
-        <div className="mt-8">
+        <div>
             <div className="w-full h-8 border-4 border-primary-orange rounded-lg flex items-center justify-start p-8 font-['700'] text-xl ">
                 {title}
             </div>
