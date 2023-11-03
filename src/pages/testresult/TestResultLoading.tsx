b751ab816c09eba0d9c4bdcef75889a0173cbdff
sonyujin <jainayj804998@gmail.com>
2023-11-03 12:19:23 +0900

    Added TestResultLoading.tsx

---
+++ src/pages/testresult/TestResultLoading.tsx
@@ -0,0 +1,14 @@
+export function TestResultLoading() {
+    return (
+      <div className="w-full">
+            <div className="font-['800'] text-2xl mt-16 ">
+            모글리님의
+            <div>생활 유형을 분석 중이에요</div>
+            </div>
+            <div className="font-['600'] text-xl mt-8 ">잠시만 기다려 주세요</div>
+            <div className="mt-8 w-full">
+              <img src={process.env.PUBLIC_URL + '/puang1.png'} alt="puang1" className="w-full h-auto" />
+          </div>
+      </div>
+    )
+}
