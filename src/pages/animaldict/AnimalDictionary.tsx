f0ac25a87418896508f373b2717477c34efbcf17
sonyujin <jainayj804998@gmail.com>
2023-11-06 16:02:01 +0900

    Update AnimalDictDetail.tsx

--- src/pages/animaldict/AnimalDictionary.tsx
+++ src/pages/animaldict/AnimalDictionary.tsx
@@ -88,9 +88,11 @@ const animalIcon = tw.rotary({
         },
 })
 interface AnimalLinkCardProps {
+    animal : string
     type : string
     color: GetVariants<typeof animalIcon>
     picture:JSX.Element;
+    
 }
 const here = () => {
    return (
@@ -99,10 +101,10 @@ const here = () => {
     </div>
    )
 }
-const AnimalLinkCard = ({ color, picture, type }: AnimalLinkCardProps) => {
+const AnimalLinkCard = ({ color, picture, type, animal }: AnimalLinkCardProps) => {
     return (
         <div>
-            <Link to={`/animaldictdetail/${type}`}className={`${animalLinkCard.class}`}>
+            <Link to={`/animaldictdetail/${animal}`}className={`${animalLinkCard.class}`}>
                 <div className={animalIcon.class(color)}>{picture}</div>
                 <div className="font-['700']">{type}</div>
             </Link>
@@ -120,7 +122,7 @@ export function AnimalDictionary() {
             </div>
             <div  className="grid grid-cols-2 grid-rows-4 gap-5 items-center justify-center mt-4">
                 {ANIMAL_LIST.map((animal) => (
-                    <AnimalLinkCard key={animal.animal} color={animal.color} picture={animal.picture} type={animal.type} />
+                    <AnimalLinkCard key={animal.animal} color={animal.color} picture={animal.picture} type={animal.type} animal={animal.animal}/>
                 ))}
             </div>
         </div>
