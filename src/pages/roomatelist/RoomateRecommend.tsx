a785bdc0ec69d7825e66ee4ac65eb1fd567697b8
sonyujin <jainayj804998@gmail.com>
2023-11-06 21:52:02 +0900

    Update RoomateRecommend.tsx: add aniroomie

--- src/pages/roomatelist/RoomateRecommend.tsx
+++ src/pages/roomatelist/RoomateRecommend.tsx
@@ -1,5 +1,13 @@
 export function RoomateRecommend() {
     return (
-        <div>추천한 룸메이트를 보여주는 페이지입니다.</div>
+        <div>
+            <div className="flex flex-row items-center justify-between">
+                <img src={process.env.PUBLIC_URL + '/aniroomie.png'} alt="roomie" width="80px"  />
+                <div className="text-2xl ml-4">
+                    <span className="font-['700']">모글리</span>님의
+                    <div>매칭결과입니다.</div>
+                </div>
+            </div>
+        </div>
     )
 }
