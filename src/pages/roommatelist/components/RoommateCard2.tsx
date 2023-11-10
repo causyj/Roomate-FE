41756cecd3e974fb7cfea691bf922f408f771092
sonyujin <jainayj804998@gmail.com>
2023-11-10 17:44:25 +0900

    Update RoomateCard2.tsx: make back of card

--- src/pages/roommatelist/components/RoommateCard2.tsx
+++ src/pages/roommatelist/components/RoommateCard2.tsx
@@ -3,7 +3,11 @@ import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import { blue, yellow } from "@mui/material/colors";
 import Star from '@mui/icons-material/StarRounded';
-import Chat from '@mui/icons-material/StarRounded';
+import Chat from '@mui/icons-material/ForumRounded';
+import FavoriteOutlined from "@mui/icons-material/FavoriteOutlined"
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder"
+import { Link } from "react-router-dom";
+import { Divider } from "@mui/material";
 interface RoomateCardProps {
     disableFlip?: boolean
 }
@@ -22,7 +26,7 @@ const FrontDetail = () => {
         <Star sx={{color : yellow[500]}} />
         </div>
     </div>
-    <div className="font-['700'] text-primary-gray text-xxs">블루미르홀 308관/4인실</div>
+    <div className="font-['700'] text-primary-gray text-3xs">블루미르홀 308관/4인실</div>
         </div>
     )
 }
@@ -54,26 +58,54 @@ const CardFront = ({isFrontView} : CardFrontBackProps) => {
     </section>
     )
 }
+const TypeAtAGlance = () =>{
+    return (
+      <div className="flex flex-col py-1">
+          <div className="flex flex-row justify-center mt-1">
+            <div className="flex flex-col font-['800'] text-2xs ml-3 mr-3 mb-2">
+                <div>생체 리듬</div>
+                <div>흡연 여부</div>
+                <div >소음 민감도</div>
+                <div>온도 민감도</div>
+                <div >외출/외박</div>
+                <div >청결도</div>
+                <div>잠버릇</div>
+            </div>
+            <div className="flex flex-col  font-['800'] text-primary-gray text-2xs">
+                <div className="">아침형</div>
+                <div className="">비흡연자</div>
+                <div className=" text-primary-orange flex-col"> 
+                    <div className="flex flex-row mt-[-6px] mb-[-9px]">
+                    <FavoriteBorder sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/>
+                    </div>
+                    <div className="flex flex-row ">
+                    <FavoriteBorder sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/><FavoriteOutlined sx={{width:'10px'}}/>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <Link to={'/typeresult'} className="font-['700'] text-xxs ml-2 text-primary-gray flex justify-center mt-[-4px]">더 자세히 보기 →</Link>
+      </div>
+    )
+}  
 const CardBack = ({isFrontView} : CardFrontBackProps) => {
     return (
         <section
-        className={`group absolute inset-0 z-0 flex h-full w-full flex-col items-start justify-between rounded-xl bg-[#F7F8FB] px-2 py-3 transition duration-300 ease-in-out ${
+        className={`group absolute inset-0 z-0 flex h-full w-full flex-col items-start justify-between rounded-xl bg-[#F7F8FB] px-2 py-1.5 transition duration-300 ease-in-out ${
             isFrontView === false ? 'opacity-0 rotate-y-180' : 'z-10 opacity-100 rotate-y-0'
         }`}
     >
        <div className="flex flex-col">
             <div className="flex flex-row">
-                <div className="flex flex-col">
-                    <div className="font-['700'] text-bold text-xl">모글리님</div>
-                    <div className="font-['700'] text-primary-gray text-xxs">블루미르홀 308관/4인실</div>
-                </div>
-                <div className="flex flex-row">
-                     <div className="">
-                        <Star sx={{color : yellow[500]}} />
-                    </div>
-
-                </div>
+                    <div className="font-['700'] text-bold text-xl ml-2">모글리님</div>
+                    <Link to={"/chat"} className="mt-1 ml-4">
+                    <Chat />
+                    </Link>
             </div>
+            <div className="font-['700'] text-primary-gray text-3xs ml-2 mt-[-4px] mb-1">블루미르홀 308관/4인실</div>
+            <Divider />
+            <TypeAtAGlance />
         </div>
         </section>
     )
