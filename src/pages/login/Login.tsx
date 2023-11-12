72e661006874a41bed9afd3265ff95db5c67e1bf
sonyujin <jainayj804998@gmail.com>
2023-11-12 13:30:43 +0900

    Added TextField.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
     return (
-        <div>로그인하는 페이지입니다.</div>
+        <div className="flex flex-col ">
+            <Link to='/register'> 
+                회원가입
+            </Link>
+            <div>
+                <img src={process.env.PUBLIC_URL + '/roomie.png'} alt="monkey" style={{width : '50px'}}/>
+                <img src={process.env.PUBLIC_URL + '/logo.png'} alt="monkey" style={{width : '300px'}}/>
+            </div>
+            <div>
+            Aniroomie에서 나의 룸메이트도 찾고 나의 기숙사 생활 유형도 알아보세요 !
+            </div>
+        </div>
     )
 }
