f82e7a79a53fd4582ec9f4f3eddcac3b3615ab8f
sonyujin <jainayj804998@gmail.com>
2023-11-14 15:37:16 +0900

    TestPageLayout.tsx: modify UI2

--- src/pages/testpage/TestPageLayout.tsx
+++ src/pages/testpage/TestPageLayout.tsx
@@ -6,8 +6,8 @@ export function TestPageLayout() {
                 <img src={process.env.PUBLIC_URL + '/logo.png'}alt="logo"style={{width : '150px'}}/>
             </div>
             <div className="font-['500'] mt-4 text-center">해당하는 대답을 선택해 주세요!</div>
-            <div className="font-['600'] text-3xl mt-8 text-center">Q.01</div>
-            <div className="font-['600'] text-xl text-center mt-4 mb-8">전 날, 알람을 여러 개 맞추고 잔 당신, 다음 날 아침 상황은?</div>
+            <div className="font-['600'] text-3xl mt-4 text-center">Q.01</div>
+            <div className="font-['600'] text-xl text-center mt-1 mb-8">전 날, 알람을 여러 개 맞추고 잔 당신, 다음 날 아침 상황은?</div>
             <AnswerCard AnsweCardText="한 번에 바로 일어난다."/>
             <AnswerCard AnsweCardText="나도 모르겠다 이말이야. 한 번에 바로 일어난다."/>
             <AnswerCard AnsweCardText="한 번에 바로 일어난다."/>
