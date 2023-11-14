2cd22bd0dcf83453bb2f0f41962707e64502f863
sonyujin <jainayj804998@gmail.com>
2023-11-14 22:40:00 +0900

    Update DescriptiveQuestion.tsx

--- src/pages/testpage/components/AnswerCard.tsx
+++ src/pages/testpage/components/AnswerCard.tsx
@@ -1,7 +1,7 @@
 export function AnswerCard({AnsweCardText }: { AnsweCardText: string }) {
     return (
         <div>
-            <div className="w-full h-20 border-2 border-stone-500 rounded-lg duration-500 hover:bg-primary-logo hover:text-white text-primary-bg  flex items-center justify-center text-center p-8 text-['900'] text-xl mb-2 font-bold">{AnsweCardText}</div>
+            <div className="w-72 h-20 border-2 border-stone-500 rounded-lg duration-500 hover:bg-primary-logo hover:text-white text-primary-bg  flex items-center justify-center text-center p-8 text-['900'] text-xl mb-2 font-bold">{AnsweCardText}</div>
         </div>
     )
 }
