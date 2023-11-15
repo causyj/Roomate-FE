a4f413ae3d7f4b5127a9b0b3e922d41c77017ecf
sonyujin <jainayj804998@gmail.com>
2023-11-15 21:39:24 +0900

    Update RoommateRecommend.tsx: add step3

--- src/pages/roommatelist/RoommateRecommend.tsx
+++ src/pages/roommatelist/RoommateRecommend.tsx
@@ -5,6 +5,8 @@ import { Optional } from './Optional';
 import CircleIcon from '@mui/icons-material/Circle';
 import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye';
 import { Button } from '../../components/common';
+import { Loading } from '../../components/Loading/Loading';
+import { useState, useEffect } from 'react';
 const Intro1 = ({ onClick }: { onClick: () => void }) => {
   return(
       <div className="flex flex-col items-center mt-8">
@@ -26,8 +28,23 @@ const Intro1 = ({ onClick }: { onClick: () => void }) => {
       </div>
   )
 }
+export const Intro2 = ({ onClick }: { onClick: () => void })  => {
+  return (
+      <div className="flex flex-col items-center justify-center font-['700']">
+          <div className="px-8 text-xl">더 많은 사용자의 유입을 위해, 기숙사 합격통보 날의 일주일 뒤부터 매칭을 시작합니다</div>
+          <div className="mt-8 mb-4">
+              <img src={process.env.PUBLIC_URL + '/calendar.png'} alt="monkey" style={{width : '200px'}}/>
+          </div>
+          <div>
+              매칭이 시작되면 알림으로 알려드릴게요 :)
+          </div>
+   
+      </div>
+  )
+}
 export function RoommateRecommend() {
     const [step, setStep] = React.useState(0);
+    const [loading, setLoading] = useState(true);
       const handleButtonClick= () =>{
         setStep(step+1);
       }
@@ -37,20 +54,11 @@ export function RoommateRecommend() {
          {step === 0 && <RoommateOption onClick={handleButtonClick} />}
          {step === 1 && <Optional onClick={handleButtonClick}/>}
          {step === 2 && <Intro1 onClick={handleButtonClick}/>} 
-         {/* <RoommateRecommendPanel />  */}
+         {/* 원래 이 페이지여야 하지만, 우선 제외 
+         {step === 3 && <Intro2 onClick={handleButtonClick}/>}  
+         */} 
+         {step === 3 && <RoommateRecommendPanel />} 
+         
       </div>
   )
 }
-
-
-// const [step, setStep] = useState(0);
-//   const handleButtonClick= () =>{
-//     setStep(step+1);
-//   }
-//     return (
-//     <div>
-//          {step === 0 && <Intro1 onClick={handleButtonClick} />}
-//          {step === 1 && <Intro2 onClick={handleButtonClick}/>}
-//          {step === 2 && <TestPageLayout />}
-//     </div>
-//     )
