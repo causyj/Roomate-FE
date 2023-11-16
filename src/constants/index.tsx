d8b840e960e36f772ebe7ec71c17055095d1b08b
sonyujin <jainayj804998@gmail.com>
2023-11-16 20:02:19 +0900

    Update AnimalType.tsx: delete description property

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -3,55 +3,47 @@ export const ANIMAL_DATA : Record<AnimalType['animal'],{
     type : string
     color : 'rose' | 'yellow' | 'orange' | 'green' | 'purple' | 'sky' | 'pink' | 'gray' 
     picture : JSX.Element;
-    descriptoin : string,
+    // descriptoin : string,
 }> = {
-    PolarFox : {
+    ArcticFox : {
         type : '북극여우',
         color : 'rose',
         picture :  <img src={process.env.PUBLIC_URL + '/PolarFox.png'} width={'80px'}/>,
-        descriptoin : 'sdf'
     },
     Rabbit : {
         type : '토끼',
         color : 'orange',
         picture :  <img src={process.env.PUBLIC_URL + '/Rabbit.png'}width={'70px'}/>,
-        descriptoin : 'sdf'
     },
     Quokka : {
         type : '쿼카',
         color : 'yellow',
         picture :  <img src={process.env.PUBLIC_URL + '/Quokka.png'} width={'80px'}/>,
-        descriptoin : 'sdf'
     },
     PolarBear : {
         type : '북극곰',
         color : 'green',
         picture :  <img src={process.env.PUBLIC_URL + '/PolarBear.png'}width={'80px'}/>,
-        descriptoin : 'sdf'
     },
     Cat : {
         type : '고양이',
         color : 'purple',
         picture :  <img src={process.env.PUBLIC_URL + '/Cat.png'} width={'80px'}/>,
-        descriptoin : 'sdf'
     },
     Penguin : {
         type : '펭귄',
         color : 'sky',
         picture :  <img src={process.env.PUBLIC_URL + '/Penguin.png'} width={'90px'}/>,
-        descriptoin : 'sdf'
     },
-    Puppy : {
+    Dog : {
         type : '강아지',
         color : 'pink',
         picture :  <img src={process.env.PUBLIC_URL + '/Puppy.png'} width={'80px'}/>,
-        descriptoin : 'sdf'
     },
     Wolf : {
         type : '늑대',
         color : 'gray',
         picture :  <img src={process.env.PUBLIC_URL + '/Wolf.png'}width={'80px'}/>,
-        descriptoin : 'sdf'
     },
 
 
--- src/interface/AnimalType.tsx
+++ src/interface/AnimalType.tsx
@@ -1,4 +1,4 @@
 export type AnimalType = {
-    animal: 'PolarFox' | 'Rabbit' | 'Quokka' | 'PolarBear' | 'Cat' | 'Penguin' | 'Puppy' | 'Wolf' 
+    animal: 'ArcticFox' | 'Rabbit' | 'Quokka' | 'PolarBear' | 'Cat' | 'Penguin' | 'Dog' | 'Wolf' 
     animalKor : '북극여우' | '토끼' | '쿼카'| '북극곰' | '고양이' | '펭귄' |  '강아지' | '늑대' 
 }
