3cb0d6b32420c299b2f53824f1d368e9d2efa7a5
sonyujin <jainayj804998@gmail.com>
2023-11-27 00:05:13 +0900

    Update Home.tsx: make a frame

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,67 +1,15 @@
-import { Link } from 'react-router-dom';
 
-export function Home() {
+
+  
+  
+  
+  export const Home = () => {
     return (
-        <div className='flex flex-col'>
-            <div className='font-bold'>세상에 이런 폰트가 d나오다니 천재인듯</div>
-            
-            <div className="font-['100']">Hi this is test For eng.ver </div>
-            
-            <div className="font-['400']">안녕 서비스임7</div>
-            <div className="font-['500']">안녕 서비스임7</div>
-            <div className="font-['600']">안녕 서비스임7</div>
-            <div className="font-['700']">안녕 서비스임7</div>
-            <div className="font-['800']">안녕 서비스임7</div>
-            <div className="font-['900']">안녕 서비스임7</div>
-            
-            <div className='flex flex-col'>
-            <Link to='/'>
-                <div className="font-['100'] ">100-home</div>
-            </Link>
-            <Link to='/Login'>
-                <div className="font-['200'] ">200-login</div>
-            </Link>
-            <Link to='/Register'>
-                <div className="font-['300'] ">300-register</div>
-            </Link>
-            <Link to='/TestPage'>
-                <div className="font-['400']">400-textpage</div>
-            </Link>
-            <Link to='/ResultHome'>
-                <div className="font-['500'] ">500-resulthome</div>
-            </Link>
-            <Link to='/TypeResult'>
-                <div className="font-['500'] ">500-typeresult</div>
-            </Link>
-            <Link to='/animaldict'>
-                <div className="font-['600'] ">600-animaldic</div>
-            </Link>
-            <Link to='/verify'>
-                <div className="font-['700'] ">700-verfiy</div>
-            </Link>
-            <Link to='/chat'>
-                <div className="font-['800'] ">800-chat</div>
-            </Link>
-            <Link to='/roommateOption'>
-                <div className="font-['900'] ">900-roomateOption</div>
-            </Link>
-            <Link to='/roommatelist'>
-                <div className="font-['900'] ">900-roomatelist</div>
-            </Link>
-            <Link to='/group'>
-                <div className="font-['900'] ">900-GroupList</div>
-            </Link>
-            <Link to='/notice'>
-                <div className="font-['900'] ">900-Notice</div>
-            </Link>
-            <Link to='/otherpeople'>
-                <div className="font-['900'] ">900-otherpeople</div>
-            </Link>
-            <Link to='/secondexample'>
-                <div className="font-['900'] ">900-secondexample</div>
-            </Link>
-            
-            </div>
-       </div>
-    )
-}
+      <div className="w-full fixed" >
+        <div className=" h-[20%] w-full bg-primary-logo fixed top-[-1px] left-0 z-1"></div>
+        <div className="h-full w-full bg-white z-2 mt-[60px] fixed rounded-t-3xl ">hhhhhhhhhhh</div>
+       
+      </div>
+    );
+  };
+  
