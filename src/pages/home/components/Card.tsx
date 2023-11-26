cab64d6628a4d2488a8351574ed6d921ebfa219e
sonyujin <jainayj804998@gmail.com>
2023-11-27 02:56:34 +0900

    Update Card.tsx: fix error-zindex

--- src/pages/home/components/Card.tsx
+++ src/pages/home/components/Card.tsx
@@ -20,11 +20,13 @@ export const Card: React.FC<CardProps> = ({ image, text, description1, descripti
   return (
     <div className="flex flex-row">
       <div
-        className={`h-60 w-[200px]  rounded-2xl bg-primary-ivory shadow-2xl flex flex-col p-3 justify-center items-center text-center  ${
+      style={{zIndex:2}}
+        className={`h-60 w-[200px] rounded-2xl bg-primary-ivory shadow-2xl flex flex-col p-3 justify-center items-center text-center  ${
           showExtraContent ? 'cursor-pointer' : ''
         }`}
         onClick={toggleExtraContent}
-      >
+        
+       >
         <img src={process.env.PUBLIC_URL + `/${image}.png`} alt="logo" style={{ width: '65px' }} />
         <div className="font-['700'] text-3xl text-primary-bg mt-2">{text}</div>
         <div className="font-['700'] text-sm text-primary-gray mt-4 ">{description1}</div>
@@ -32,7 +34,7 @@ export const Card: React.FC<CardProps> = ({ image, text, description1, descripti
       </div>
       {showExtraContent && (
         <Link to={`${link}`}>
-            <div className="h-60 w-20 z-1 rounded-r-2xl bg-primary-logo shadow-2xl flex flex-col p-3 justify-center items-end text-center ml-[-20px]">
+            <div style={{zIndex:1}} className="h-60 w-20 rounded-r-2xl bg-primary-logo shadow-2xl flex flex-col p-3 justify-center items-end text-center ml-[-20px]">
                 <ArrowForwardIosIcon sx={{width:'35px', height:'35px', color:'white'}}/>
             </div>
         </Link>
