c0eb2a9739069577b654fafc4496f18e2c346bba
sonyujin <jainayj804998@gmail.com>
2023-11-28 15:23:31 +0900

    Update Verify.tsx

--- src/pages/verify/Verify.tsx
+++ src/pages/verify/Verify.tsx
@@ -1,5 +1,38 @@
+import { Uploader } from "uploader"; // Installed by "react-uploader".
+import { UploadDropzone } from "react-uploader";
+import { Button } from "../../components/common";
+// Initialize once (at the start of your app).
+const uploader = Uploader({
+    apiKey: "free" // Get production API keys from Bytescale
+  });
+  
+  // Configuration options: https://www.bytescale.com/docs/upload-widget/frameworks/react#customize
+  const options = { multi: true };
+  const ImageUpload = () => (
+    <UploadDropzone uploader={uploader}
+                    options={options}
+                    onUpdate={files => alert(files.map(x => x.fileUrl).join("\n"))}
+                    width="600px"
+                    height="200px" />
+  );
 export function Verify() {
     return (
-        <div>기숙사 합격을 인증 하는 페이지입니다.</div>
+        <div className="flex flex-col items-center w-full">
+             <div className="flex items-center justify-center mb-4">
+                <img src={process.env.PUBLIC_URL + '/logo.png'}alt="logo"style={{width : '130px'}}/>
+            </div>
+            <div className="font-['700'] text-m flex flex-col text-center ">
+                <div className="">추천을 받기 위해서는</div>
+                <div><span className="text-blue-600">기숙사 합격 인증</span>이 필요해요!</div>
+            </div>
+            <div className="mt-2 text-center mx-auto">
+                <div className="font-['700']">예시)</div>
+                <img src={process.env.PUBLIC_URL + '/dormitory.jpg'}alt="logo"style={{width : '150px'}} className="text-center mx-auto"/>
+                <ImageUpload/>
+            </div>
+           <div className="font-['400'] text-center mx-auto text-sm">합격인증 완료까지 <span className="font-['700']">1일~2일</span>정도 소요될 수 있습니다. </div>
+           <div className="font-['700'] text-center mx-auto text-sm mb-3 ">합격 인증이 완료되면 알림으로 알려드릴게요 :)</div>
+           <Button buttonText="기숙사 합격 인증 받기"></Button>
+        </div>
     )
 }
