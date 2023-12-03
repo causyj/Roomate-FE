48608f7404d6ca52ed2595fbb17370410529b19b
sonyujin <jainayj804998@gmail.com>
2023-12-03 10:28:00 +0900

    Update Chat.tsx: add chatting list

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export function Chat() {
   return (
-    <div className="max-w-[413px] flex flex-col justify-center items-center w-full">
-      <div className="w-full font-['700'] text-3xl flex justify-start  bg-blue-50 p-8 z-10">
-        채팅목록
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className=" max-w-[413px] w-full h-24 font-['700'] text-3xl flex bg-white justify-start fixed top-0 px-9 items-center "  style={{zIndex:200}}>
+        <div className="">채팅목록</div>
       </div>
-      <div className="w-full">
+      <div className="w-full mt-[72px]">
         <Link to='/roommatelist' className="flex items-start">
           <div className="max-w-[413px] w-full h-16 bg-gray-100 rounded-2xl">
             <div className="flex flex-col items-center justify-start p-3">
@@ -21,19 +21,178 @@ export function Chat() {
         </Link>
       </div>
 
-      <div className="w-full flex flex-row">
-        <div>
-          <Stack direction="row" spacing={2}>
-            <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
-          </Stack>
+     <div className="flex flex-col overflow-y-auto gap-4 mt-4">
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
         </div>
-        <div className="flex flex-col ml-4 py-0.5">
-          <div className="font-['700'] text-lg ">모글리님</div>
-          <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+        </div>
+        <div className="w-full flex flex-row">
+                    <div>
+                    <Stack direction="row" spacing={2}>
+                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                    </Stack>
+                    </div>
+                    <div className="flex flex-col ml-4 py-0.5">
+                        <div className="font-['700'] text-lg ">모글리님</div>
+                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                    </div>
+                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
         </div>
 
-        <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
-      </div>
+
+     </div>
     </div>
   );
 }
