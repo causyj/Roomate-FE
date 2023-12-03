32d96712acd9337457f58dbf37da575f5865aedb
sonyujin <jainayj804998@gmail.com>
2023-12-03 10:39:26 +0900

    Update ChatList.tsx:add route for chatting page

--- src/pages/chat/ChatList.tsx
+++ src/pages/chat/ChatList.tsx
@@ -2,6 +2,7 @@ import { Avatar, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export function ChatList() {
+  const nickname = "모글리"
   return (
     <div className="flex flex-col justify-center items-center w-full">
       <div className=" max-w-[413px] w-full h-24 font-['700'] text-3xl flex bg-white justify-start fixed top-0 px-9 items-center "  style={{zIndex:200}}>
@@ -22,18 +23,20 @@ export function ChatList() {
       </div>
 
      <div className="flex flex-col overflow-y-auto gap-4 mt-4">
-        <div className="w-full flex flex-row">
-                    <div>
-                    <Stack direction="row" spacing={2}>
-                        <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
-                    </Stack>
-                    </div>
-                    <div className="flex flex-col ml-4 py-0.5">
-                        <div className="font-['700'] text-lg ">모글리님</div>
-                        <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
-                    </div>
-                    <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
-        </div>
+        <Link to={`${nickname}`}>
+          <div className="w-full flex flex-row">
+                      <div>
+                      <Stack direction="row" spacing={2}>
+                          <Avatar alt="Remy Sharp" sx={{ bgcolor: 'pink', width: 50, height: 50 }} src={process.env.PUBLIC_URL + '/cat.png'} />
+                      </Stack>
+                      </div>
+                      <div className="flex flex-col ml-4 py-0.5">
+                          <div className="font-['700'] text-lg ">{nickname}님</div>
+                          <div className="font-['700'] text-xs text-primary-gray">예민한 고양이 / 혹은 마지막 채팅 내용</div>
+                      </div>
+                      <div className="font-['700'] text-xs text-primary-gray text-xxs text-end ml-auto mr-1 mt-2">오전 11:38</div>
+          </div>
+        </Link>
         <div className="w-full flex flex-row">
                     <div>
                     <Stack direction="row" spacing={2}>
