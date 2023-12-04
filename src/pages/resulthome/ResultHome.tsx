cfb864060e23d61f8d60292a8250e983f28dcdb7
sonyujin <jainayj804998@gmail.com>
2023-12-04 21:55:42 +0900

    Update GoodBad.tsx : api connect

--- src/pages/resulthome/ResultHome.tsx
+++ src/pages/resulthome/ResultHome.tsx
@@ -9,10 +9,21 @@ import { useEffect, useState } from "react"
 import { AnimalType } from "../../interface/AnimalType"
 
 //api : 닉네임 / 동물 유형 / 
+interface AnimalDataProps{
+  animal : string ;
+  sensitive : boolean;
+}
+interface DescriplDataProps{
+  features : string;
+  wellMatchedRoommates : string[];
+  incompatibleRoommates : string[];
+  
+}
 export const ResultHome =() => {
   const [nickname, setNickname] = useState('');
-  const [animal, setAnimal] = useState('');
-
+  const [animalData, setAnimalData] = useState<AnimalDataProps | null>(null);
+  const [descripData, setDescripData] = useState<DescriplDataProps | null>(null);
+//  닉네임
   useEffect(()=>{
     const nicknameData = async () => {
       try {
@@ -34,8 +45,9 @@ export const ResultHome =() => {
     };
     nicknameData();
   },[]);
+  // 동물
   useEffect(()=>{
-    const animalData = async () => {
+    const AnimalData = async () => {
       try {
         const response = await fetch(`http://aniroomi-env.eba-rj7upyms.ap-northeast-2.elasticbeanstalk.com/animal`, {
           method: 'GET',
@@ -44,7 +56,7 @@ export const ResultHome =() => {
   
         if (response.ok) {
           const data = await response.json();
-          setAnimal(data); // 새로운 카드 정보 설정
+          setAnimalData(data); // 새로운 카드 정보 설정
           
         } else {
           console.error('Failed to nicknameData data : ', response.status, response.statusText);
@@ -53,76 +65,42 @@ export const ResultHome =() => {
         console.error('Failed to nicknameData data : ', error);
       }
     };
-    animalData();
+    AnimalData();
+  },[]);
+  //동물 한줄 소개 & good or bad
+  useEffect(()=>{
+    const DescripData = async () => {
+      try {
+        const response = await fetch(`http://aniroomi-env.eba-rj7upyms.ap-northeast-2.elasticbeanstalk.com/style/animal`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+  
+        if (response.ok) {
+          const data = await response.json();
+          setDescripData(data); // 새로운 카드 정보 설정
+          
+        } else {
+          console.error('Failed to DesripData data : ', response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error('Failed to DesripData data : ', error);
+      }
+    };
+    DescripData();
   },[]);
 
-    const result = "polarBear";
-    const animalInfo = ANIMAL_DATA[result];
-    const animalColor = animalInfo.color; //orange
-    
-   
-          const animalIconStyles = {
-          yellow: {
-              color : 'border-yellow-500', 
-          },
-          rose: {
-              color : 'border-rose-500', 
-          },
-          orange: {
-              color : 'border-orange-500', 
-          },
-          green: {
-              color : 'border-green-500', 
-          },
-          sky: {
-              color : 'border-sky-500', 
-          },
-          purple: {
-              color : 'border-purple-500', 
-          },
-          gray: {
-              color : 'border-gray-500', 
-          },
-          pink: {
-              color : 'border-pink-600', 
-          },
-      }
-      const heartIconStyles = {
-          yellow: {
-              color : 'text-yellow-500', 
-          },
-          rose: {
-              color : 'text-rose-500', 
-          },
-          orange: {
-              color : 'text-orange-500', 
-          },
-          green: {
-              color : 'text-green-500', 
-          },
-          sky: {
-              color : 'text-sky-500', 
-          },
-          purple: {
-              color : 'text-purple-500', 
-          },
-          gray: {
-              color : 'text-gray-500', 
-          },
-          pink: {
-              color : 'text-pink-600', 
-          },
-      }
-      const animalIconStyle = animalIconStyles[animalColor];
-      const heartIconStyle = heartIconStyles[animalColor];
+
+ 
     const noise = 3;
     const temperature = 3;
     const outgoing = 1;
     const clean = 5;
     const sleep = 5;
-    const animals = 'penguin'; 
-    const color = getAnimalColor(animals);
-    const colorRGB = getAnimalColorRGB(animals);
+   
+    const color = getAnimalColor(animalData?.animal as AnimalType['animal']);
+    const colorRGB = getAnimalColorRGB(animalData?.animal as AnimalType['animal']);
+    const adv = animalData?.sensitive == true ? "예민한" : "무던한"
     console.log(color);
     return (
         <div>
@@ -130,11 +108,13 @@ export const ResultHome =() => {
            <div className="flex w-full items-center justify-center mb-4">
                     <img src={process.env.PUBLIC_URL + '/logo.png'}alt="logo"style={{width : '120px'}}/>
             </div>
-            
-            <div className="text-center text-2xl font-['800'] items-center justify-center">
-                <div className='mb-4 mt-8'>{nickname}님의 생활 유형은</div>
-                <span className={`text-4xl text-['900'] text-${color}-500`} >둔감한 토끼</span> 
-            </div>
+            {animalData &&
+             <div className="text-center text-2xl font-['800'] items-center justify-center">
+             <div className='mb-4 mt-8'>{nickname}님의 생활 유형은</div>
+             <span className={`text-4xl text-['900'] text-${color}-500`}>{adv} {animalData.animal}</span> 
+         </div>
+            }
+           
 
             <div className="mt-6 flex justify-center mb-8 ">
             <img src={process.env.PUBLIC_URL + `/rabbit.png`} alt={`rabbit`} style={{width : '150px'}}/>
@@ -146,9 +126,12 @@ export const ResultHome =() => {
 
            
             <TitleBox title={'나의 동물 유형 한줄 소개'} animalColor={color}/>
+            {descripData && 
             <div className=" font-['600'] p-7 text-l text-primary-gray">
-            상쾌한 아침을 맞이하는 아침형 인간으로, 혼자만의 시간을 중요하게 생각하고 겨울보단 여름을 좋아하는 성향이 있다.
+               {descripData.features}
             </div>
+            }
+            
             <Link to='/animaldict'>
                 <div className="w-full h-16 bg-gray-100 rounded-2xl mt-[-12px] mb-6">
                 <div className="flex flex-col items-center justify-start p-3">
@@ -170,11 +153,21 @@ export const ResultHome =() => {
                 clean={5}
                 sleep={5}
             />
-
-           <GoodBad color={color} colorRGB={colorRGB}/>
+            
+            {descripData && 
+             <GoodBad 
+             linecolor={color} 
+              colorRGB={colorRGB} 
+              good1={descripData.wellMatchedRoommates[0]} 
+              good2={descripData.wellMatchedRoommates[1]}
+              bad1={descripData.incompatibleRoommates[0]}
+              bad2={descripData.incompatibleRoommates[1]}
+              />
+            }
+          
     
             
-            <TitleBox title={'김푸앙님의 생활 유형 상세보기'} animalColor={color}/>
+            <TitleBox title={'나의 생활 유형 상세보기'} animalColor={color}/>
             <ResultDetail />
             
             <Link to='/roommatelist'>
--- src/pages/resulthome/components/GoodBad.tsx
+++ src/pages/resulthome/components/GoodBad.tsx
@@ -1,56 +1,66 @@
-import { ANIMAL_DATA } from "../../../constants";
+import { ANIMAL_DATA, getAnimalColor, getAnimalColorRGB } from "../../../constants";
 import FavoriteOutlined from "@mui/icons-material/FavoriteOutlined"
 import BrokenHeart from "@mui/icons-material/HeartBrokenOutlined"
 import { Avatar, Stack } from "@mui/material";
+import { AnimalType } from "../../../interface/AnimalType";
 type GoodBadProps = {
-    color : string | undefined;
+    linecolor : string | undefined;
     colorRGB: string | undefined;
+    good1 : string;
+    good2 : string;
+    bad1 : string;
+    bad2 : string;
 }
-export const GoodBad = ({color, colorRGB} : GoodBadProps) =>{
-    const result = "rabbit";
+
+// const color = getAnimalColor(animalData?.animal as AnimalType['animal']);
+export const GoodBad = ({linecolor, colorRGB, good1, good2, bad1, bad2} : GoodBadProps) =>{
+    const good1bg = getAnimalColorRGB(good1  as AnimalType['animal']);
+    const good2bg = getAnimalColorRGB(good2  as AnimalType['animal']);
+    const bad1bg = getAnimalColorRGB(bad1  as AnimalType['animal']);
+    const bad2bg = getAnimalColorRGB(bad2  as AnimalType['animal']);
 
     return (
         <div>
             <div className="flex flex-row justify-between mb-6">
                 <div className="flex flex-col text-center items-center">
-                    <div className={`w-40 mr-4 border-4 border-${color}-500 rounded-lg flex items-center justify-center p-1`}>
+                    <div className={`w-40 mr-4 border-4 border-${linecolor}-500 rounded-lg flex items-center justify-center p-1`}>
                         <img src={process.env.PUBLIC_URL + '/good.png'} alt="morning"  />
                     </div>
                     <div className="mr-3 mt-1 font-['700'] text-sm">이런 룸메와 잘 맞아요</div>
                     <div className="mr-4 font-['700'] flex flex-col justify-center items-start  ">
                         <div className="flex flex-row mt-4 items-center ">
                         <Stack direction="row" spacing={2}>
-                            <Avatar alt="Remy Sharp" sx={{bgcolor: colorRGB, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/rabbit.png`} />
+                            <Avatar alt="Remy Sharp" sx={{bgcolor: good1bg, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/${good1}.png`} />
                         </Stack> 
-                                <div className="ml-2">토끼</div>
+                                <div className="ml-2">{good1}</div>
                         </div>
                         <div className="flex flex-row  mt-1 items-center">
                         <Stack direction="row" spacing={2}>
-                            <Avatar alt="Remy Sharp" sx={{bgcolor:colorRGB, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/rabbit.png`} />
+                            <Avatar alt="Remy Sharp" sx={{bgcolor : good2bg, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/${good2}.png`} />
                         </Stack> 
-                            <div className="ml-2 ">북극곰</div>
+                            <div className="ml-2 ">{good2}</div>
                         </div>
                        
                     </div>
                 </div>
 
                 <div className="flex flex-col text-center items-center justify-center">
-                      <div className={`w-40 mr-4 border-4 border-${color}-500 rounded-lg flex items-center justify-center p-1`}>
+                      <div className={`w-40 mr-4 border-4 border-${linecolor}-500 rounded-lg flex items-center justify-center p-1`}>
                         <img src={process.env.PUBLIC_URL + '/bad.png'} alt="morning"  />
                       </div>
                       <div className="text-center mt-1 mr-3 font-['700'] text-sm">이런 룸메와는 조금 힘들어요</div>
                       <div className="mr-4 font-['700'] flex flex-col justify-center items-start  ">
                           <div className="flex flex-row font-['700']  mt-4">
                           <Stack direction="row" spacing={2}>
-                            <Avatar alt="Remy Sharp" sx={{bgcolor: colorRGB, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/rabbit.png`} />
+                            <Avatar alt="Remy Sharp" sx={{bgcolor: bad1bg, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/${bad1}.png`} />
                         </Stack> 
-                                <div className="ml-2">늑대</div>
+                                <div className="ml-2">{bad1}</div>
                         </div>
                         <div className="flex flex-row text-center font-['700'] items-center justify-center mt-1">
                         <Stack direction="row" spacing={2}>
-                            <Avatar alt="Remy Sharp" sx={{bgcolor: colorRGB, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/rabbit.png`} />
+                            <Avatar alt="Remy Sharp" sx={{bgcolor: bad2bg, width: 25, height: 25}} src={process.env.PUBLIC_URL + `/${bad2}.png`} />
                         </Stack> 
-                            <div className="ml-2 ">쿼카</div>
+                            <div className="ml-2 ">{bad2}</div>
                         </div>
                    
                       </div>
