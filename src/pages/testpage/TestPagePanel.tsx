9c2b10d3314616bb217c434dac3386ff289123d7
sonyujin <jainayj804998@gmail.com>
2023-12-05 17:30:18 +0900

    Update TestPagePanel.tsx : confirm quesiont move when press button

--- src/pages/testpage/TestPagePanel.tsx
+++ src/pages/testpage/TestPagePanel.tsx
@@ -1,19 +1,77 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnswerCard } from "./components/AnswerCard";
 import { DescriptiveQuestion } from './DescriptiveQuestion';
 import { TEST_LIST } from '../../constants';
-
+type answerProps = {
+  index : number;
+}
 export function TestPagePanel() {
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(0);
+
+
+const [bedtimeScore, setbedtimeScore] = useState(0);
+const [wakeupScore, setwakeupScore] = useState(0);
+const [wakeupSensitivity, setwakeupSensitivity] = useState(0);
+const [cleaningScore, setcleaningScore] = useState(0);
+const [cleaningSensitivity, setcleaningSensitivity] = useState(0);
+const [foodScore, setfoodScore] = useState(0);
+const [foodSensitivity, setfoodSensitivity] = useState(0);
+const [cigaretteScore, setcigaretteScore] = useState(0);
+const [studyScore, setstudyScore] = useState(0);
+const [studySensitivity, setstudySensitivity] = useState(0);
+const [notebookScore, setnotebookScore] = useState(0);
+const [notebookSensitivity, setnotebookSensitivity] = useState(0);
+const [alarmScore, setalarmScore] = useState(0);
+const [alarmSensitivity, setalarmSensitivity] = useState(0);
+const [latestudyScore, setlatestudyScore] = useState(0);
+const [latestudySensitivity, setlatestudySensitivity] = useState(0);
+const [snoringScore, setsnoringScore] = useState(0);
+const [snoringSensitivity, setsnoringSensitivity] = useState(0);
+const [friendlyScore, setfriendlyScore] = useState(0);
+const [inhomeScore, setinhomeScore] = useState(0);
+const [inhomeSensitivity, setinhomeSensitivity] = useState(0);
+const [coldOrHot, setcoldOrHot] = useState(0);
+const [summerOrWinter, setsummerOrWinter] = useState(0);
+
+  useEffect(() => {
+    console.log(`저장한 답변 ${selectedAnswer}`);
+  }, [selectedAnswer]); // selectedAnswer가 변경될 때만 실행
+
+  const handleAnswerSelect = ({ index }: answerProps) => {
+    // 선택한 답변을 상태에 저장
+    setSelectedAnswer(index+1);
+  };
 
   const handleNextPage = () => {
     // 다음 페이지로 이동
-    if (questionIndex < TEST_LIST.length-1 ) {
-      setQuestionIndex((prevIndex) => prevIndex + 1);
+    if (questionIndex < TEST_LIST.length - 1) {
+      let nextQuestionIndex;
+      //questionIndex+1 : 질문 번호
+      //selectedAnswer : 답변 번호
+      switch (questionIndex+1) {
+        case 1:
+          if (selectedAnswer === 1) {
+            nextQuestionIndex = 5;
+          } else {
+            // 다른 조건에 따라 처리
+            nextQuestionIndex = questionIndex + 1;
+          }
+          break;
+        // 다른 특정한 case에 대한 처리 추가 가능
+
+        default:
+          nextQuestionIndex = questionIndex + 1;
+          break;
+      }
+
+      setQuestionIndex(nextQuestionIndex);
+      setSelectedAnswer(0); // 선택한 답변 초기화
     } else {
-        return <DescriptiveQuestion />;
+      return <DescriptiveQuestion />;
     }
   };
+
 
   return (
     <div>
@@ -28,7 +86,7 @@ export function TestPagePanel() {
       <div className="font-['600'] text-xl text-center mt-1 mb-8">{TEST_LIST[questionIndex].question}</div>
       <div>
       {(TEST_LIST[questionIndex].answer).map((answer, index) => (
-    <AnswerCard key={index} AnsweCardText={answer} />
+    <AnswerCard index={index} AnswerCardText={answer} onClick={() => handleAnswerSelect({index})} />
   ))}
 </div>
       <div className="font-['700'] flex items-center justify-between mb-20 mt-12">
--- src/pages/testpage/components/AnswerCard.tsx
+++ src/pages/testpage/components/AnswerCard.tsx
@@ -1,7 +1,21 @@
-export function AnswerCard({AnsweCardText }: { AnsweCardText: string }) {
-    return (
-        <div>
-            <div className="w-72 h-20 border-2 border-stone-500 rounded-lg duration-500 hover:bg-primary-logo hover:text-white text-primary-bg  flex items-center justify-center text-center p-8 text-['900'] text-xl mb-2 font-bold">{AnsweCardText}</div>
-        </div>
-    )
+// AnswerCard.tsx
+import React from 'react';
+
+interface AnswerCardProps {
+  index: number;
+  AnswerCardText: string;
+  onClick?: (index: number) => void;  // onClick의 매개변수로 index 추가
 }
+
+export const AnswerCard = ({ index, AnswerCardText, onClick } : AnswerCardProps) => {
+  return (
+    <div>
+      <button
+        className="w-72 h-20 border-2 border-stone-500 rounded-lg duration-500 hover:bg-primary-logo hover:text-white text-primary-bg  flex items-center justify-center text-center p-8 text-['900'] text-xl mb-2 font-bold"
+        onClick={() => onClick && onClick(index)}  // key를 매개변수로 전달
+      >
+        {AnswerCardText}
+      </button>
+    </div>
+  );
+};
